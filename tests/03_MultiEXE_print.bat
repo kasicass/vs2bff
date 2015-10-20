a48d8af052742f013009bea46b0a91fe493ba113
kasicass <kasicass@gmail.com>
2015-10-20 10:44:40 +0800

    MultiEXE test ok

---
+++ tests/03_MultiEXE_print.bat
@@ -0,0 +1,6 @@
+copy /Y ..\bin\Debug\bff_printargs.exe ..\bin\Debug\cl.exe
+copy /Y ..\bin\Debug\bff_printargs.exe ..\bin\Debug\lib.exe
+copy /Y ..\bin\Debug\bff_printargs.exe ..\bin\Debug\link.exe
+copy /Y ..\bin\Debug\bff_printargs.exe ..\bin\Debug\rc.exe
+
+msbuild.exe /t:Rebuild /p:ExecutablePath="%~dp0..\bin\Debug" 03_MultiEXE\MultiEXE.sln
